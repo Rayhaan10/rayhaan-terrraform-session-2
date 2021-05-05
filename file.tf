08f1dd80330bdbb70687f96d993d51abee142f7f
rayhaan <rayhaan10@gmail.com>
2021-05-05 08:48:09 -0500

    added version constraint

--- file.tf
+++ file.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "hashicorp/aws"
+      version = ">= 0.13"
+    }
+  }
+}
+
 resource "random_pet" "rayray" {
   prefix = var.prefix
 }
